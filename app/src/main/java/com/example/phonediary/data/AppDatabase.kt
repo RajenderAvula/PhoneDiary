c2b8bb09b3ed147c630739c5c30ee9ec371cfc9a
RajenderAvula <rajenderavula28@gmail.com>
2026-09-10 09:30:31 +0530

    Update AppDatabase.kt

--- app/src/main/java/com/example/phonediary/data/AppDatabase.kt
+++ app/src/main/java/com/example/phonediary/data/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [LogEntry::class, DiaryEntry::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
